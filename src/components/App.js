4798c16c4413f7b37b5e6a223b8464a1600a0549
dankoknad <dankomilutinovic@gmail.com>
2018-05-23 12:10:30 +0200

    adding GoogleMap component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import data from '../data.json'
+import GoogleMap from './GoogleMap'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.mapEl = React.createRef();
+  }
+
+  componentDidMount() {
+    // console.log(this.mapEl)
+    // this.mapEl.current.innerHTML = "hello world"
+
+    // this.initMap()
+
+
+
+  }
+
   render() {
     return (
       <div className="">
-        <h1>Modern Architecture</h1>  
+        <h1>Modern Architecture</h1>
+        <div id="map" ref={this.mapEl}></div>
+        <GoogleMap />
         <pre>{JSON.stringify(data, null, 2)}</pre>   
       </div>
     );
---
+++ src/components/GoogleMap.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class GoogleMap extends Component {
+  render() {
+    return (
+      <div>
+        i'm GoogleMap component
+      </div>
+    )
+  }
+}
