a03f0d550eb45798d236e730b4c8741f6d5c4464
dankoknad <dankomilutinovic@gmail.com>
2018-05-23 18:14:25 +0200

    adding and styling (JS) custom markers

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -1,5 +1,6 @@
 /*global google*/
 import React, { Component } from 'react'
+import markerIcon from '../img/marker.svg'
 
 export default class GoogleMap extends Component {
   constructor(props) {
@@ -13,7 +14,7 @@ export default class GoogleMap extends Component {
 
   componentDidMount() {
     this.map = new google.maps.Map(this.mapEl, {
-      center: {lat: -34.397, lng: 150.644},
+      center: { lat: -34.397, lng: 150.644 },
       zoom: 6
     });
 
@@ -21,10 +22,19 @@ export default class GoogleMap extends Component {
   }
 
   createMarkers = (d) => {
+    const icon = {
+      url: markerIcon, // url
+      scaledSize: new google.maps.Size(50, 50), // scaled size
+      origin: new google.maps.Point(0, 0), // origin
+      anchor: new google.maps.Point(25, 50) // anchor
+    };
+
     d.map(house => {
       this.marker = new google.maps.Marker({
-        position: {lat: house.lat, lng: house.lng},
-        map: this.map
+        position: { lat: house.lat, lng: house.lng },
+        map: this.map,
+        icon: icon,
+        title: house.name
       });
     })
   }
