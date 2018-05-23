3fc80a353a8d9a78020b9a7b7ef46ffd2ef70886
dankoknad <dankomilutinovic@gmail.com>
2018-05-23 11:13:32 +0200

    testing data imported from .json file - inside UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import data from '../data.json'
 
 class App extends Component {
   render() {
     return (
       <div className="">
-        <h1>Modern Architecture</h1>     
+        <h1>Modern Architecture</h1>  
+        <pre>{JSON.stringify(data, null, 2)}</pre>   
       </div>
     );
   }
