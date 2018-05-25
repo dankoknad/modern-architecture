d726f01ec7d3f9a3bbaa570c92cfa48947812a5f
dankoknad <dankomilutinovic@gmail.com>
2018-05-25 14:31:07 +0200

    changing z-index of the selected marker

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -16,7 +16,7 @@ export default class GoogleMap extends Component {
   componentDidMount() {
     this.map = new google.maps.Map(this.mapEl, {
       center: { lat: -34.397, lng: 150.644 },
-      zoom: 6
+      zoom: 5.3
     });
 
     this.createMarkers(this.props.data)
@@ -26,23 +26,27 @@ export default class GoogleMap extends Component {
   createMarkers = (d) => {
     const icon = {
       url: markerIcon, // url
-      scaledSize: new google.maps.Size(50, 50), // scaled size
+      scaledSize: new google.maps.Size(38, 50), // scaled size
       origin: new google.maps.Point(0, 0), // origin
-      anchor: new google.maps.Point(25, 50), // anchor
-      labelOrigin: new google.maps.Point(25, 19) // label position
+      anchor: new google.maps.Point(19, 50), // anchor
+      labelOrigin: new google.maps.Point(19, 19) // label position
     };
-    var _self = this
+    const _self = this;
+    let maxZIndex = 1;
+
     d.map(house => {
       this.marker = new google.maps.Marker({
         position: { lat: house.lat, lng: house.lng },
         map: this.map,
         icon: icon,
+        zIndex: maxZIndex,
         label: String(house.id + 1),
         title: house.name,
         houseId: house.id
       });
 
       _self.marker.addListener('click', function(e) {
+        this.setZIndex(maxZIndex++);
         this.map.panTo({
           lat: this.getPosition().lat(), 
           lng: this.getPosition().lng()
