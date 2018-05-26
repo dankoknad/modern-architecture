6df261f28fa7a591514f7c40292d12af020721a8
dankoknad <dankomilutinovic@gmail.com>
2018-05-26 23:01:44 +0200

    adding typechecking for House component

--- src/components/House.js
+++ src/components/House.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 function House({house, activeHouse, selectHouse}) {
   const isActive = activeHouse && activeHouse.id === house.id ? "active" : ""
@@ -8,6 +9,12 @@ function House({house, activeHouse, selectHouse}) {
       <img src={house.img} alt={house.name}/>
     </div>
   )
+}
+
+House.propTypes = {
+  house: PropTypes.object.isRequired,
+  activeHouse: PropTypes.object.isRequired,
+  selectHouse: PropTypes.func.isRequired
 }
 
 export default House
